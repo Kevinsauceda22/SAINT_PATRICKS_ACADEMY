15f30dc7e2286bbb5a14137edcd224b3cf2d78b5
DPavon24 <pavondavid121@gmail.com>
2024-11-22 15:58:44 -0600

    Update InstitutosRoutes.js

--- backend/module/calificaciones/Routes/InstitutosRoutes.js
+++ backend/module/calificaciones/Routes/InstitutosRoutes.js
@@ -2,15 +2,23 @@ import express from 'express';
 import {
     obtenerInstitutos,
     crearInstituto,
+    actualizarinstituto,
+    eliminarinstituto,
 } from '../Controller/institutoController.js'; // Importamos las funciones del controlador
 
 const router = express.Router();
 
 
-// Ruta para obtener todos los historiales académicos
+// Ruta para obtener todos los institutos
 router.get('/instituto', obtenerInstitutos);
 
-// Ruta para crear un nuevo historial académico
+// Ruta para crear un nuevo instituto
 router.post('/crearinstituto', crearInstituto);
+
+//Ruta para actualizar un instituto
+router.put('/actualizarinstituto', actualizarinstituto);
+
+//Ruta para eliminar un instituto
+router.delete('/eliminarinstituto', eliminarinstituto)
 
 export default router;
