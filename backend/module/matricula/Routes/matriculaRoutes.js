c35666a8a6b253982a8dd286cf0499956c540803
gabzxxxz <gabrielamador469@gmail.com>
2024-11-24 04:06:34 -0600

    matricula
    cambios

--- backend/module/matricula/Routes/matriculaRoutes.js
+++ backend/module/matricula/Routes/matriculaRoutes.js
@@ -1,5 +1,15 @@
 import express from 'express';
-import { crearMatricula, obtenerMatriculas, obtenerOpcionesMatricula, obtenerHijosPorDniPadre, obtenerSeccionesPorGrado } from '../Controllers/matriculaController.js';
+import {
+  crearMatricula,
+  obtenerMatriculas,
+  obtenerOpcionesMatricula,
+  obtenerHijosPorDniPadre,
+  obtenerSeccionesPorGrado,
+  obtenerAlumnosMatriculadosPorGradoYAno,
+  obtenerMatriculasConPeriodo, 
+  obtenerAlumnosPorSeccion,
+  obtenerHorarioPorSeccion,// Importar el nuevo controlador
+} from '../Controllers/matriculaController.js';
 
 const router = express.Router();
 
@@ -14,7 +24,19 @@ router.get('/opciones', obtenerOpcionesMatricula);
 
 // Ruta para obtener los hijos asociados al DNI del padre
 router.get('/hijos/:dni_padre', obtenerHijosPorDniPadre);
+
 // Ruta para obtener secciones disponibles para un grado específico
 router.get('/secciones/:cod_grado', obtenerSeccionesPorGrado);
+
+// Nueva ruta para obtener alumnos matriculados por grado
+router.get('/alumnos/:cod_grado', obtenerAlumnosMatriculadosPorGradoYAno);
+// Ruta para obtener datos solo por año académico del período
+router.get('/matriculas-con-periodo', obtenerMatriculasConPeriodo);
+router.get('/alumnos/seccion/:cod_seccion', obtenerAlumnosPorSeccion);
+
+// Ruta para obtener el horario por sección
+router.get('/horario/:cod_seccion', obtenerHorarioPorSeccion);
+
+
 
 export default router;
