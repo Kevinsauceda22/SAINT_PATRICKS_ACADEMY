382fd09fc647fde8119f36ad86c7b138ca0ed3e7
kch2024 <krissreyes02@gmail.com>
2024-11-25 00:07:39 -0600

    Update secc_asigRoutes.js

--- backend/module/matricula/Routes/secc_asigRoutes.js
+++ backend/module/matricula/Routes/secc_asigRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { obtenerDetalleSeccionAsignatura, obtenerSecciones, obtenerDias, obtenerGradosAsignaturas,crearHorarioSeccionAsignatura, actualizarSeccionAsignatura, obtenerAsignaturasPorProfesor, obtenerAsignaturasPorSeccion
-} from '../Controllers/secc_asigController.js';
+import { obtenerDetalleSeccionAsignatura, obtenerSecciones, obtenerDias, obtenerGradosAsignaturas,crearHorarioSeccionAsignatura, actualizarSeccionAsignatura, obtenerAsignaturasyHorarios,obtenerAsignaturasPorSeccionYGrado,obtenerSeccionesPorGrado,obtenerAsignaturasPorProfesor,
+    obtenerAsignaturasPorSeccion,getDetalleSeccionAsignatura} from '../Controllers/secc_asigController.js';
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/obtener_seccion_asig/:Cod_seccion_asignatura?', obtenerDetalleSeccionAsignatura);
 
 // Ruta para obtener secciones
-router.get('/secciones', obtenerSecciones);
+router.get('/secciones/:Cod_grado/:Cod_periodo_matricula', obtenerSecciones);
 
 // Ruta para obtener secciones
 router.get('/dias', obtenerDias);
@@ -22,10 +22,22 @@ router.post('/crear_seccion_asig', crearHorarioSeccionAsignatura);
 // Ruta para actualizar una sección
 router.put('/actualizar_seccion_asig', actualizarSeccionAsignatura);
 
+// Ruta para obtener secciones
+router.get('/asignaturashorarios/:cod_seccion', obtenerAsignaturasyHorarios);
+
+// Ruta para obtener secciones por el momento no
+router.get('/asignaturasgrados/:Cod_secciones', obtenerAsignaturasPorSeccionYGrado);
+
+// Ruta para obtener secciones
+router.get('/gradosasig/:Cod_grado', obtenerSeccionesPorGrado);
+
+router.get('/detalle/:Cod_seccion_asignatura', getDetalleSeccionAsignatura);
+
 // Ruta para obtener las asignaturas por codSeccion y profesor
 router.get('/verseccionesasignaturas/:codSeccion', obtenerAsignaturasPorProfesor);
 
 // Definir la ruta para obtener asignaturas por sección
 router.get('/porseccion/:codSeccion', obtenerAsignaturasPorSeccion);
+
 
 export default router;
