b3118e873d47aa0dc0d25fe1432fd132c5081cb7
gabzxxxz <gabrielamador469@gmail.com>
2024-11-26 00:11:26 -0600

    mas cambios
    mas cambios

--- backend/module/matricula/Routes/periodomatriculaRoutes.js
+++ backend/module/matricula/Routes/periodomatriculaRoutes.js
@@ -3,7 +3,8 @@ import {
     crearPeriodoMatricula,
     obtenerPeriodoMatricula,
     actualizarPeriodoMatricula,
-    eliminarPeriodoMatricula
+    eliminarPeriodoMatricula,
+    actualizarEstadoPeriodo,
 } from '../Controllers/periodomatriculaController.js'; // Asegúrate de que esta ruta sea correcta
 
 const router = express.Router();
@@ -18,9 +19,12 @@ router.get('/periodos', obtenerPeriodoMatricula);
 router.get('/periodos/:Cod_periodo_matricula', obtenerPeriodoMatricula);
 
 // Ruta para actualizar un periodo de matrícula por Cod_periodo_matricula
-router.put('/periodos/:Cod_periodo_matricula', actualizarPeriodoMatricula); // Es necesario enviar Cod_periodo_matricula en la URL
+router.put('/periodos/:Cod_periodo_matricula', actualizarPeriodoMatricula);
 
 // Ruta para eliminar un periodo de matrícula por Cod_periodo_matricula
-router.delete('/periodos/:Cod_periodo_matricula', eliminarPeriodoMatricula); // Nueva ruta para eliminar periodo
+router.delete('/periodos/:Cod_periodo_matricula', eliminarPeriodoMatricula);
+
+// **Nueva Ruta** para actualizar el estado de un periodo de matrícula
+router.put('/estado', actualizarEstadoPeriodo); // Correctamente registrada en el router
 
 export default router;
