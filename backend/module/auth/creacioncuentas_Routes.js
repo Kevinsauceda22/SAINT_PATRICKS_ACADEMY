77b20110227926cc53390088eb8876385a8a7f83
Kevin Steven Sauceda Flores <154033755+Kevinsauceda22@users.noreply.github.com>
2024-11-26 00:10:52 -0600

    cambios para el grupo de calificaciones
    Funcion de agregar a la tabla profesores funcional y lista

--- backend/module/auth/creacioncuentas_Routes.js
+++ backend/module/auth/creacioncuentas_Routes.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { 
     crearPersonaYUsuario,
-    actualizarPasswordPrimerIngreso
+    actualizarPasswordPrimerIngreso 
 } from '../auth/creacioncuentas_Controller.js';
 import checkAuth from '../../middleware/Auth_middleware.js';
+
 
 const router = express.Router();
 
 // Rutas protegidas - requieren autenticación y permisos
-router.post('/crear-usuario', 
+router.post('/crear-usuario',
     checkAuth,
     crearPersonaYUsuario
 );
@@ -18,6 +19,8 @@ router.put('/actualizar-password-temporal',
     checkAuth,
     actualizarPasswordPrimerIngreso
 );
+
+
 
 // Ruta base: /api/personas
 export default router;
