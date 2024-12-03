21c82629297fbae9b798b553160bfdb5c500558b
DPavon24 <pavondavid121@gmail.com>
2024-12-03 14:53:43 -0600

    Update historialAcademico.js

--- backend/module/calificaciones/Routes/historialAcademico.js
+++ backend/module/calificaciones/Routes/historialAcademico.js
@@ -3,21 +3,29 @@ import {
     obtenerHistoriales,
     crearHistorial,
     actualizarHistorial,
-    eliminarHistorial
+    eliminarHistorial,
+    obtenerGradosMatricula,
+    obtenerPersonasPorGrado 
 } from '../Controller/historialAcademicoController.js'; // Importamos las funciones del controlador
 
 const router = express.Router();
 
-// Ruta para obtener todos los historiales académicos
+// Ruta para obtener todos los historiales acadÃ©micos
 router.get('/historiales', obtenerHistoriales);
+router.get('/gradosMatricula', obtenerGradosMatricula);
+router.get('/gradosMatricula/:cod_grado', obtenerPersonasPorGrado);
 
-// Ruta para crear un nuevo historial académico
+
+
+// Ruta para crear un nuevo historial acadÃ©mico
 router.post('/crearhistorial', crearHistorial);
 
-// Ruta para actualizar un historial académico
+// Ruta para actualizar un historial acadÃ©mico
 router.put('/actualizarhistorial', actualizarHistorial);
 
-// Ruta para eliminar un historial académico
+// Ruta para eliminar un historial acadÃ©mico
 router.delete('/eliminarhistorial', eliminarHistorial);
+
+
 
 export default router;
