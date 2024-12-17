43e40cd12da8ebddf2ba005ede8eff8d0ce0abd2
Geovanny <arielo_o67@hotmail.es>
2024-12-17 02:14:01 -0600

    a

--- frontend/src/views/icons/coreui-icons/CoreUIIcons.jsx
+++ frontend/src/views/icons/coreui-icons/CoreUIIcons.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { CCard, CCardBody, CCardHeader, CRow } from '@coreui/react'
 import { freeSet } from '@coreui/icons'
-import { getIconsView } from '../brands/Brands.js'
+import { getIconsView } from '../brands/Brands.jsx'
 import { DocsCallout } from 'src/components'
 
 const CoreUIIcons = () => {
--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -7,6 +7,7 @@ import { resolve } from 'path'
 export default defineConfig({
   base: './',
   build: {
+    
     outDir: 'build',
     alias: {
       '@': resolve(__dirname, 'src')
