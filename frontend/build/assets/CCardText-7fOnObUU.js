da71b2bc710559f2b8d731103b2819db51898c07
Kevin Steven Sauceda Flores <154033755+Kevinsauceda22@users.noreply.github.com>
2025-01-09 21:24:25 -0600

    Production

---
+++ frontend/build/assets/CCardText-7fOnObUU.js
@@ -0,0 +1,1 @@
+import{r as m,_ as p,R as o,a as i,c as N,P as a}from"./index-CazN4ZNT.js";var r=m.forwardRef(function(e,t){var c=e.children,s=e.as,n=s===void 0?"p":s,l=e.className,d=p(e,["children","as","className"]);return o.createElement(n,i({className:N("card-text",l)},d,{ref:t}),c)});r.propTypes={as:a.elementType,children:a.node,className:a.string};r.displayName="CCardText";export{r as C};
