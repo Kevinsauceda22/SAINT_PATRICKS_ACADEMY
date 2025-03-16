bd6b0b6e0b50cf14d84941eba06b5d59e6d4dd70
cruz.jasonperdomo@mail.com <129896103+JasonPerdomo@users.noreply.github.com>
2025-03-16 13:56:57 -0600

    Lista Personas Actual

--- frontend/src/views/pages/personas/ListaPersonas.jsx
+++ frontend/src/views/pages/personas/ListaPersonas.jsx
@@ -13,6 +13,7 @@ import {
   cilDescription,
   cilInfo,
   cilContact,
+  cilHistory,
   cilPeople,
 } from '@coreui/icons'
 import { useNavigate } from 'react-router-dom'
@@ -90,7 +91,7 @@ const ListaPersonas = () => {
   const [personaToDelete, setPersonaToDelete] = useState({})
   const [searchTerm, setSearchTerm] = useState('')
   const [currentPage, setCurrentPage] = useState(1)
-  const [recordsPerPage, setRecordsPerPage] = useState(5)
+  const [recordsPerPage, setRecordsPerPage] = useState(10)
   const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false)
 
   const [tipoPersona, setTipoPersona] = useState([])
@@ -154,6 +155,11 @@ const ListaPersonas = () => {
   const abrirContactoModal = (personas) => {
     console.log('Persona seleccionada en el componente origen:', personas); // Verifica que los datos estén presentes
     navigate('/contacto', { state: { personaSeleccionada: personas } });
+  };
+
+  const abrirHistoricoProcedenciaModal = (personas) => {
+    console.log('Persona seleccionada en el componente origen:', personas); // Verifica que los datos estén presentes
+    navigate('/ListaHistoricoProc', { state: { personaSeleccionada: personas } });
   };
 
 
@@ -1360,13 +1366,23 @@ return (
                   >
                     <CIcon icon={cilPeople} />{' '}
                   </CButton>
-                  <CButton
-                  color="primary"
-                  onClick={() => abrirContactoModal(persona)}
-                  style={{ marginLeft: '10px' }}
-                >
-                  <CIcon icon={cilContact} /> {/* Cambia esto por el ícono adecuado */}
-                </CButton>
+                    {
+                      tipoPersona.find((tipo) => tipo.Cod_tipo_persona === persona.cod_tipo_persona)?.Tipo === 'ESTUDIANTE' ? (
+                        <CButton
+                          onClick={() => abrirHistoricoProcedenciaModal(persona)}
+                          style={{ marginLeft: '10px', backgroundColor: '#90EE90', borderColor: '#90EE90' }}
+                        >
+                          <CIcon icon={cilHistory} />
+                        </CButton>
+                      ) : (
+                        <CButton
+                          onClick={() => abrirContactoModal(persona)}
+                          style={{ marginLeft: '10px', backgroundColor: '#007bff', borderColor: '#007bff' }}
+                        >
+                          <CIcon icon={cilContact} />
+                        </CButton>
+                      )
+                    }
                 </div>
               </CTableDataCell>
             </CTableRow>
@@ -3032,7 +3048,4 @@ return (
   )
 }
 export default ListaPersonas
-
-
-
 
