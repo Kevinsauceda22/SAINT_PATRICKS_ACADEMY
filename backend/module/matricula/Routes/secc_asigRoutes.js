dc1f4c4b486efaff4293dfabbdf55fe4acc39300
cruz.jasonperdomo@mail.com <129896103+JasonPerdomo@users.noreply.github.com>
2025-03-24 13:01:58 -0600

    Mas lista horario

--- backend/module/matricula/Routes/secc_asigRoutes.js
+++ backend/module/matricula/Routes/secc_asigRoutes.js
@@ -1,42 +1,36 @@
 import express from 'express';
-import { obtenerDetalleSeccionAsignatura, 
-         obtenerSecciones, 
-         obtenerDias, 
-         obtenerGradosAsignaturas,
-         actualizarSeccionAsignatura, 
-         obtenerAsignaturasyHorarios,
-         obtenerAsignaturasPorSeccionYGrado,
-         getDetalleSeccionAsignatura, 
-         obtenerAsignaturasPorGradoYSeccion
+import {obtenerTodasSeccionesAsignaturas, 
+        obtenerTodasAsignaturas,
+        obtenerTodasSecciones,
+        obtenerTodosGrados,
+        crearSeccionAsignatura, 
+        actualizarSeccionAsignatura, 
+        eliminarSeccionAsignatura,
 } from '../Controllers/secc_asigController.js';
 
 const router = express.Router();
 
-// Ruta para obtener todas las secciones y asignaturas o una específica
-router.get('/obtener_seccion_asig/:Cod_seccion_asignatura?', obtenerDetalleSeccionAsignatura);
 
 // Ruta para obtener secciones
-router.get('/secciones/:Cod_grado/:Cod_periodo_matricula', obtenerSecciones);
+router.get('/verSeccionesAsignaturas', obtenerTodasSeccionesAsignaturas);
 
-// Ruta para obtener secciones
-router.get('/dias', obtenerDias);
+// Ruta para obtener Asignaturas
+router.get('/verAsignaturas', obtenerTodasAsignaturas);
 
-// Ruta para obtener secciones
-router.get('/grados_asignaturas', obtenerGradosAsignaturas);
+//Rutas para obtener secciones
+router.get('/verSecciones', obtenerTodasSecciones);
+
+//Rutas para obtener Grados
+router.get('/verGrados', obtenerTodosGrados);
 
 // Ruta para actualizar una sección
-router.put('/actualizar_seccion_asig', actualizarSeccionAsignatura);
+router.post('/crearSeccionAsignatura', crearSeccionAsignatura);
 
 // Ruta para obtener secciones
-router.get('/asignaturashorarios/:cod_seccion', obtenerAsignaturasyHorarios);
+router.put('/actualizarSeccionAsignatura/:Cod_seccion_asignatura', actualizarSeccionAsignatura);
 
-// Ruta para obtener secciones por el momento no
-router.get('/asignaturasgrados/:Cod_secciones', obtenerAsignaturasPorSeccionYGrado);
+// Ruta para eliminar 
+router.delete('/eliminarSeccionAsignatura/:Cod_seccion_asignatura', eliminarSeccionAsignatura);
 
-// Detalles
-router.get('/detalle/:Cod_seccion_asignatura', getDetalleSeccionAsignatura);
-
-// Ruta 
-router.get('/asignaturas/:Cod_seccion?', obtenerAsignaturasPorGradoYSeccion);
 
 export default router;
