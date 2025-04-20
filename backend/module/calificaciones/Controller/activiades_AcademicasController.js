b6f9c8fe952108b43d7040f01f6a067f143de94c
Manu19999 <148603095+Manu19999@users.noreply.github.com>
2025-04-19 21:06:38 -0600

    Update activiades_AcademicasController.js

--- backend/module/calificaciones/Controller/activiades_AcademicasController.js
+++ backend/module/calificaciones/Controller/activiades_AcademicasController.js
@@ -246,7 +246,7 @@ export const obtenerActividadesPorAsignatura = async (req, res) => {
                 Fechayhora_Fin,
                 Valor,
                 Cod_secciones,
-                Cod_grados_asignaturas
+                Cod_grados_asignaturass
             ]
         );
 
